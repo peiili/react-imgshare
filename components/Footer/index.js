3a6b3ee4584504242ec3aa0c8cb777a73049edf5
peiili <1017935195@qq.com>
2022-04-04 01:07:25 +0800

    fix:添加跳转链接

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,12 @@ import style from './style.module.css'
 function Footer() {
   return (
     <div className={style.footer}>
-      <div>Copyright©www.dlsjf.top|粤ICP备18024512号</div>
+      <div>Copyright©www.dlsjf.top|
+        <a href='https://beian.miit.gov.cn/#/Integrated/index'
+            target="_blank">
+         粤ICP备18024512号
+          </a>
+         </div>
     </div>
   )
 }
