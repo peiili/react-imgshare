8347dd1608709e44eb7d0799bf5d7baf63d02f31
燕红立 <yanhongli@369exhibition.com>
2022-04-11 16:21:34 +0800

    fix: 修改域名显示

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,12 +3,12 @@ import style from './style.module.css'
 function Footer() {
   return (
     <div className={style.footer}>
-      <div>Copyright©www.dlsjf.top|
+      <div>Copyright©{location.host}|
         <a href='https://beian.miit.gov.cn/#/Integrated/index'
-            target="_blank">
-         粤ICP备18024512号
-          </a>
-         </div>
+          target="_blank">
+          粤ICP备18024512号
+        </a>
+      </div>
     </div>
   )
 }
