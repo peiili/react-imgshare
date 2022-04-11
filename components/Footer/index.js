b5e6054b73a65e33f91cdbfbf08d0928b20384c7
燕红立 <yanhongli@369exhibition.com>
2022-04-11 17:14:15 +0800

    修改copyright

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './style.module.css'
 function Footer() {
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    setLocation(window.location)
+  }, []);
   return (
     <div className={style.footer}>
-      <div>Copyright©{location.host}|
+      <div>Copyright©{location ? location.host : ''}|
         <a href='https://beian.miit.gov.cn/#/Integrated/index'
           target="_blank">
           粤ICP备18024512号
