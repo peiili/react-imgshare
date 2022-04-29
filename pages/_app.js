c9d1fcdbaf8017c358952c904a01c58d90b1a308
燕红立 <yanhongli@369exhibition.com>
2022-04-29 17:00:33 +0800

    fix: 优化关键词

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,9 @@ export default function App({ Component, pageProps }) {
                 <title>三味书屋</title>
                 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"></link>
                 <link rel="apple-touch-icon" href="/favicon.ico" />
-                <meta name="keywords" content="壁纸,博客,前端,照片排版,bing壁纸,bing,网站搭建,博客搭建,个人博客" />
+                <meta name="keywords" content="壁纸,照片排版,bing壁纸,bing,个人博客" />
                 <meta name="description" content="分享bing优秀的壁纸，让用户不再因找不到好看的壁纸而烦恼,同时会分享一些自己的生活，开发一些排版小工具，分享一些好的博客" />
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
             </Head>
             <Script
                 src="https://hm.baidu.com/hm.js?e6a4a66be15383cdb75c16f0da432f3b"
