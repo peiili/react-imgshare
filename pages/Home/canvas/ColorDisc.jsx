5049a118c3425aa53c01e648ced2e32c0104047a
燕红立 <yanhongli@369exhibition.com>
2022-06-02 15:14:34 +0800

    feat:限制调色盘移动范围

--- pages/Home/canvas/ColorDisc.jsx
+++ pages/Home/canvas/ColorDisc.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import styles from './style.module.css'
-
 let start = false,
     /**
      * 鼠标相对客户端点击位置
@@ -9,13 +8,18 @@ let start = false,
     /**
      * 当前控制块所在的位置
      */
-    currentPosition = { x: 10, y: 10 }
+    currentPosition = { x: 10, y: 10 },
+    /**
+     * 最终位置
+     */
+    resultedPosition = { x: 0, y: 0 }
 const ColorDisc = function (props) {
+    const control = useRef()
     const { colors = ['#000000'], onClick } = props
     const [activeColor, setActiveColor] = useState(colors[0]);
     const [position, setPosition] = useState({
-        top: 10,
-        left: 10
+        top: currentPosition.y,
+        left: currentPosition.x
     });
     const onClickSide = function (event) {
         const { clientX, clientY } = event
@@ -28,9 +32,21 @@ const ColorDisc = function (props) {
         if (!start) return;
         const topLength = clientY - startPosition.y
         const leftLength = clientX - startPosition.x
+        resultedPosition.x = leftLength + currentPosition.x
+        resultedPosition.y = topLength + currentPosition.y
+        if (resultedPosition.y < 0) {
+            resultedPosition.y = 0
+        } else if (resultedPosition.y > (window.innerHeight - control.current.offsetHeight)) {
+            resultedPosition.y = window.innerHeight - control.current.offsetHeight
+        }
+        if (resultedPosition.x < 0) {
+            resultedPosition.x = 0
+        } else if (resultedPosition.x > (window.innerWidth - control.current.offsetWidth)) {
+            resultedPosition.x = window.innerWidth - control.current.offsetWidth
+        }
         setPosition({
-            top: topLength + currentPosition.y,
-            left: leftLength + currentPosition.x
+            top: resultedPosition.y >= 0 ? resultedPosition.y : 0,
+            left: resultedPosition.x >= 0 ? resultedPosition.x : 0
         })
     }
 
@@ -40,6 +56,7 @@ const ColorDisc = function (props) {
         currentPosition.y = position.top
     }, [position])
     useEffect(() => {
+        console.log(control.current.offsetWidth);
         const sideBar = document.getElementById('sideBar')
         sideBar.addEventListener('mousedown', onClickSide)
         document.addEventListener('mousemove', onMove)
@@ -52,7 +69,7 @@ const ColorDisc = function (props) {
     }, [onEnd]);
     return (
         <>
-            <div className={styles.control} style={{ top: position.top + 'px', left: position.left + 'px' }}>
+            <div ref={control} className={styles.control} style={{ top: position.top + 'px', left: position.left + 'px' }}>
                 <div id='sideBar' className={styles.sideBar}></div>
                 <div className={styles.discbox}>
                     {
