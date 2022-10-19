dbd1968c2239f8b1b7f685acde36f824e3665be2
燕红立 <yanhongli@369exhibition.com>
2022-10-19 15:23:38 +0800

    完善富文本编辑器

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 import Script from 'next/script'
 import Head from 'next/head'
+import { Button } from 'antd'
 
 class Editmd extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: '',
-      password: '',
-      selectValue: '选项3',
-      react: false,
-      redux: false,
-      mobx: false,
+      testEditormdView1: null
     }
+    this.getHTML = this.getHTML.bind(this)
   }
 
   componentDidMount() {
-    window.editormd("test-editormd-view2", {
-      width: "90%",
-      height: 720,
-      toc: true,
-      //atLink    : false,    // disable @link
-      //emailLink : false,    // disable email address auto link
-      todoList: true,
-      path: '/static/js/'
-    });
+    this.setState({
+      testEditormdView1: window.editormd("test-editormd-view2", {
+        width: "90%",
+        height: 720,
+        toc: true,
+        imageUpload: true,          // Enable/disable upload
+        imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
+        saveHTMLToTextarea: true,
+        todoList: true,
+        emoji: false,
+        path: '/static/js/'
+      })
+    })
   }
+  getHTML() {
+    // console.log(this.state);
+    console.log(this.state.testEditormdView1.getMarkdown());
+    console.log(this.state.testEditormdView1.getHTML());
+  }
+
   render() {
     return (
       <>
@@ -53,6 +60,18 @@ class Editmd extends Component {
         />
         <Script
           strategy="beforeInteractive"
+          src="/static/js/raphael.min.js"
+        />
+        <Script
+          strategy="beforeInteractive"
+          src="/static/js/underscore.min.js"
+        />
+        <Script
+          strategy="beforeInteractive"
+          src="/static/js/sequence-diagram.min.js"
+        />
+        <Script
+          strategy="beforeInteractive"
           src="/static/js/prettify.min.js"
         />
         <Script
@@ -67,6 +86,7 @@ class Editmd extends Component {
           <div id="test-editormd-view2">
             <textarea id="append-test" style={{ display: 'none' }} defaultValue={'###科学公式 TeX(KaTeX)'}></textarea>
           </div>
+          <Button onClick={() => this.getHTML()}>获取html</Button>
         </div>
       </>
     )
